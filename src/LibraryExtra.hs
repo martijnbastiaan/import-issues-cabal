a99f4186d7f701fba374e25cd530e3dd68ea4870
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-12-31 12:48:06 +0100

    Init

---
+++ src/LibraryExtra.hs
@@ -0,0 +1,1 @@
+module LibraryExtra where
---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import System.Process
+import System.IO
+
+main :: IO ()
+main = do
+  (Just stdin, _, _, ph) <- createProcess ((proc "ghci" []){std_in = CreatePipe})
+  hPutStrLn stdin "import LibraryExtra"
+  hFlush stdin
+  hPutStrLn stdin ":q"
+  hFlush stdin
+  waitForProcess ph
+  pure ()
