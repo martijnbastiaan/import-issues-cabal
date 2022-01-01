6201fa073cbb24fe1cf26ef96b65988af58fb6f4
Martijn Bastiaan <martijn@hmbastiaan.nl>
2022-01-01 14:46:35 +0100

    Remove obsolete extra lib

--- src/Main.hs
+++ src/Main.hs
@@ -8,8 +8,6 @@ main = do
   (Just stdin, _, _, ph) <- createProcess ((proc "ghci" []){std_in = CreatePipe})
   hPutStrLn stdin "import Library"
   hFlush stdin
-  hPutStrLn stdin "import LibraryExtra"
-  hFlush stdin
   hPutStrLn stdin ":q"
   hFlush stdin
   waitForProcess ph
